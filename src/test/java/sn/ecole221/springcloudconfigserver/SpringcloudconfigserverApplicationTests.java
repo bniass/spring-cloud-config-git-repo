0b4f72ec2dafce1d7ce9424f483a565e036baa9d
ibnaliou <papadjibyniang@gmail.com>
2020-11-25 11:13:14 +0000

    eeee

---
+++ src/test/java/sn/ecole221/springcloudconfigserver/SpringcloudconfigserverApplicationTests.java
@@ -0,0 +1,17 @@
+package sn.ecole221.springcloudconfigserver;
+
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.validation.constraints.AssertTrue;
+
+@SpringBootTest
+class SpringcloudconfigserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
